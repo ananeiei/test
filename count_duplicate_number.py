cb99cdb3ef7f59a7bfb4bc974d9d8175e99619a0
Anan Ungpansattawong <74874750+ananeiei@users.noreply.github.com>
2022-12-27 21:56:45 +0700

    update-Print the count for each number

--- count_duplicate_number.py
+++ count_duplicate_number.py
@@ -1,8 +1,14 @@
 def count_duplicate_number (list_number):
+  
+  #Count the number of occurrences of each number
   list_number.sort()
   my_dict = {i:list_number.count(i) for i in list_number}
-  return my_dict
+  
+  # Print the count for each number
+  for number, count in my_dict.items():
+    print(f"{number}: {count}")
+
 
 
 input_number = [2,3,4,6,2,4,4,1]
-print(count_duplicate_number(input_number))
+count_duplicate_number(input_number)
